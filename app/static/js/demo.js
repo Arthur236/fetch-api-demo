2304b0130dbbacaf7ba2201dbbe361d348c1f06f
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2018-08-01 14:24:30 +0300

    add logic

--- app/static/js/demo.js
+++ app/static/js/demo.js
@@ -1,41 +1,38 @@
-class Post
-
-  const addPost = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          title: document.getElementById('title').value,
-          body: document.getElementById('body').value
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      })
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
-      })
-  }
-
-  const getPosts = () => {
-    // GET request
-    const url2 = 'https://jsonplaceholder.typicode.com/posts';
-
-    fetch(url2)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      let i;
-      for (i = 0; i < data.length; i++) {
-        console.log(data.length)
-        document.getElementById('post-list').innerHTML += `<div>
-        <h4><i>${data[i].title}</i></h4>
-        <p>${data[i].body}</p>
-        </div><hr/>`
+const addPost = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: document.getElementById('title').value,
+        body: document.getElementById('body').value
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
       }
     })
-    .catch(error => {
-      console.log(error);
-    });
-  }
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+    })
+}
+
+const getPosts = () => {
+  // GET request
+  const url2 = 'https://jsonplaceholder.typicode.com/posts';
+
+  fetch(url2)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let i;
+    for (i = 0; i < data.length; i++) {
+      console.log(data.length)
+      document.getElementById('post-list').innerHTML += `<div>
+      <h4><i>${data[i].title}</i></h4>
+      <p>${data[i].body}</p>
+      </div><hr/>`
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
